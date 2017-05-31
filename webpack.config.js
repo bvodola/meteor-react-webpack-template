dbafc80466bdf80d63cac1beaf371a40278cca1a
Brunno Vodola <bvodola@gmail.com>
2017-05-31 12:15:34 -0300

    Changing the source directory from /client to /src and updating README.md

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: './client/index.html',
+  template: './src/index.html',
   filename: 'index.html',
   inject: 'body'
 })
